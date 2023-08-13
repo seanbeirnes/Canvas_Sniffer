764a22cda641d16332dde5e1c1e25d52d64332fa
seanbeirnes <127370575+seanbeirnes@users.noreply.github.com>
2023-08-13 00:11:38 -0400

    Moved header to a separate componenet.

---
+++ app/components/layout/header.py
@@ -0,0 +1,39 @@
+from components.frames.header_frame import header_frame
+from components.labels.static_header_text_label import static_header_text_label
+from components.labels.static_header_text_value_label import static_header_text_value_label
+
+class Header():
+    def __init__(self, master, app_status) -> None:
+        self.master = master
+        self.canvasInstance = app_status.canvas_instance_url
+        self.status = app_status.status
+
+    def init(self) -> None:
+        self.frame_header = header_frame(self.master)
+
+        self.label_canvasInstance = static_header_text_label(
+            master=self.frame_header,
+            text="Canvas Instance: "
+            )
+        
+        self.label_canvasInstance_value = static_header_text_value_label(
+            master=self.frame_header,
+            text=self.canvasInstance
+        )
+        
+        self.label_status = static_header_text_label(
+            master=self.frame_header,
+            text="Status: "
+            )
+        
+        self.label_status_value = static_header_text_value_label(
+            master=self.frame_header,
+            text=self.status
+        )
+
+    def update(self, app_status):
+        self.canvasInstance = app_status.canvas_instance_url
+        self.status = app_status.status
+
+        self.label_canvasInstance_value.configure(text = self.canvasInstance)
+        self.label_status_value.configure(text = self.status)
--- app/controllers/app_view_controller.py
+++ app/controllers/app_view_controller.py
@@ -4,10 +4,7 @@ from config.global_config import Global as G
 from views.app.search_courses_view import Search_Courses_View
 
 # Componenet import
-from components.layout.header_frame import header_frame
-
-from components.labels.static_header_text_label import static_header_text_label
-from components.labels.static_header_text_value_label import static_header_text_value_label
+from components.layout.header import Header
 
 
 # Controlls the main view of the app. Includes the header and main layout.
@@ -15,28 +12,8 @@ from components.labels.static_header_text_value_label import static_header_text_
 class App_View_Controller():
     def __init__(self, master, app_data) -> None:
         self.master = master
-        self.canvasInstance = app_data.app_status.canvas_instance_url
-        self.status = app_data.app_status.status
+        self.app_data = app_data
     
     def init(self):
-        self.frame_header = header_frame(self.master)
-
-        self.label_canvasInstance = static_header_text_label(
-            master=self.frame_header,
-            text="Canvas Instance: "
-            )
-        
-        self.label_canvasInstance_value = static_header_text_value_label(
-            master=self.frame_header,
-            text=self.canvasInstance
-        )
-        
-        self.label_status = static_header_text_label(
-            master=self.frame_header,
-            text="Status: "
-            )
-        
-        self.label_status_value = static_header_text_value_label(
-            master=self.frame_header,
-            text=self.status
-        )
+        header = Header(master=self.master, app_status=self.app_data.app_status)
+        header.init()
