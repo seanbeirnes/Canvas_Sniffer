a47480e0498416ef5a21c7cd3b54e5d221b6da43
seanbeirnes <127370575+seanbeirnes@users.noreply.github.com>
2023-08-13 01:13:17 -0400

    Made main layout configure function have higher weights on grid.

--- app/components/layout/config_main_layout.py
+++ app/components/layout/config_main_layout.py
@@ -2,6 +2,6 @@ import customtkinter as ctk
 
 def config_main_layout(master) -> None:
     master.columnconfigure(0,minsize=152, weight=1)
-    master.columnconfigure(1, weight=50)
+    master.columnconfigure(1, weight=1000)
     master.rowconfigure(0, minsize=40, weight=1)      
-    master.rowconfigure(1, minsize=560, weight=50)
+    master.rowconfigure(1, minsize=560, weight=1000)
